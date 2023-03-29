7fe8eafb611cb38adfe7457f92b914c62b2eca7d
dTRANSMISSION <transmission@transsmission-mac.local>
2023-03-28 22:42:29 -0700

    refactor: reorder

--- go_fundamentals/5_structs_methods_interfaces/shapes.go
+++ go_fundamentals/5_structs_methods_interfaces/shapes.go
@@ -2,6 +2,10 @@ package shapes
 
 import "math"
 
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -11,22 +15,14 @@ func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
 type Circle struct {
 	Radius float64
 }
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
-}
-
-type Shape interface {
-	Area() float64
-}
-
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
-}
-
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
 }

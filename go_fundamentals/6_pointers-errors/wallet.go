b1095a144062fddb5b823960d29ac7d3573d20cd
dTRANSMISSION <transmission@transsmission-mac.local>
2023-08-03 22:26:57 -0700

    refactor: implement Stringer

--- go_fundamentals/6_pointers-errors/wallet.go
+++ go_fundamentals/6_pointers-errors/wallet.go
@@ -8,8 +8,15 @@ type Wallet struct {
   balance Bitcoin
 }
 
+type Stringer interface {
+  String() string
+}
+
+func (b Bitcoin) String() string {
+  return fmt.Sprintf("%d BTC", b)
+}
+
 func (w *Wallet) Deposit(amount Bitcoin) {
-  fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
   w.balance += amount
 }
 

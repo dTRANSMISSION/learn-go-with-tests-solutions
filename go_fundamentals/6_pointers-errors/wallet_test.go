02a37dce1f8b1de20739054c5e77cf72eb353248
dTRANSMISSION <transmission@transsmission-mac.local>
2023-11-26 18:41:18 -0800

    test: add assertNoError

--- go_fundamentals/6_pointers-errors/wallet_test.go
+++ go_fundamentals/6_pointers-errors/wallet_test.go
@@ -7,12 +7,15 @@ func TestWallet(t *testing.T) {
   t.Run("deposit", func(t *testing.T) {
     wallet := Wallet{}
     wallet.Deposit(Bitcoin(10))
+
     assertBalance(t, wallet, Bitcoin(10))
   })
 
   t.Run("withdraw with funds", func(t *testing.T) {
     wallet := Wallet{balance: Bitcoin(20)}
-    wallet.Withdraw(Bitcoin(10))
+    err := wallet.Withdraw(Bitcoin(10))
+
+    assertNoError(t, err)
     assertBalance(t, wallet, Bitcoin(10))
   })
 
@@ -43,5 +46,12 @@ func assertError(t testing.TB, got, want error) {
 
   if got != want {
     t.Errorf("got %q, want %q", got, want)
+  }
+}
+
+func assertNoError(t testing.TB, got error, want error) {
+  t.Helper()
+  if got != nil {
+    t.Fatal("got an error but didn't want one")
   }
 }

804a1f1c246529dbcb040921567197465ae3fa75
dTRANSMISSION <transmission@transsmission-mac.local>
2023-11-26 18:02:39 -0800

    implement assertError test helper

--- go_fundamentals/6_pointers-errors/wallet.go
+++ go_fundamentals/6_pointers-errors/wallet.go
@@ -1,6 +1,9 @@
 package wallet
 
-import "fmt"
+import (
+  "fmt"
+  "errors"
+)
 
 type Bitcoin int
 
@@ -20,8 +23,14 @@ func (w *Wallet) Deposit(amount Bitcoin) {
   w.balance += amount
 }
 
-func (w *Wallet) Withdraw(amount Bitcoin) {
- w.balance -= amount
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+
+  if amount > w.balance {
+    return errors.New("oh no")
+  }
+
+  w.balance -= amount
+  return nil
 }
 
 func (w *Wallet) Balance() Bitcoin {

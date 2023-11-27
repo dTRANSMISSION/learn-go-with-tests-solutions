6587ebba8601930c7f5cd2beeac592b4d9cbf80f
dTRANSMISSION <transmission@transsmission-mac.local>
2023-11-26 18:24:07 -0800

    refactor: move test helpers to dedicated func, ErrInsufficientFunds var

--- go_fundamentals/6_pointers-errors/wallet_test.go
+++ go_fundamentals/6_pointers-errors/wallet_test.go
@@ -4,33 +4,13 @@ import "testing"
 
 func TestWallet(t *testing.T) {
 
-  assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
-    t.Helper()
-    got := wallet.Balance()
-
-    if got != want {
-      t.Errorf("got %s want %s", got, want)
-    }
-  }
-
-  assertError := func(t testing.TB, got error, want string) {
-    t.Helper()
-    if got == nil {
-      t.Fatal("wanted an error but didn't get one")
-    }
-
-    if got.Error() != want {
-      t.Errorf("got %q, want %q", got, want)
-    }
-  }
-
   t.Run("deposit", func(t *testing.T) {
     wallet := Wallet{}
     wallet.Deposit(Bitcoin(10))
     assertBalance(t, wallet, Bitcoin(10))
   })
 
-  t.Run("withdraw", func(t *testing.T) {
+  t.Run("withdraw with funds", func(t *testing.T) {
     wallet := Wallet{balance: Bitcoin(20)}
     wallet.Withdraw(Bitcoin(10))
     assertBalance(t, wallet, Bitcoin(10))
@@ -41,7 +21,27 @@ func TestWallet(t *testing.T) {
     wallet := Wallet{startingBalance}
     err := wallet.Withdraw(Bitcoin(100))
 
-    assertError(t, err, "cannot withdraw, insufficient funds")
+    assertError(t, err, ErrInsufficientFunds)
     assertBalance(t, wallet, startingBalance)
   })
+}
+
+func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
+  t.Helper()
+  got := wallet.Balance()
+
+  if got != want {
+    t.Errorf("got %q want %q", got, want)
+  }
+}
+
+func assertError(t testing.TB, got, want error) {
+  t.Helper()
+  if got == nil {
+    t.Fatal("wanted an error but didn't get one")
+  }
+
+  if got != want {
+    t.Errorf("got %q, want %q", got, want)
+  }
 }
